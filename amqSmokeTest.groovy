e610d6cdc021b5e7b1868e5d6728eccdec7c6fc3
Kevin Earls <kevin@kevinearls.com>
2016-04-13 10:21:32 +0200

    Extracted os dependent code

--- amqSmokeTest.groovy
+++ amqSmokeTest.groovy
@@ -5,99 +5,106 @@
 
 stage 'define tools'
 // Get the tools and override EVs which may be set on the node
-def M2_HOME = tool 'maven-3.2.3'  // TODO fix 3.3.3 on windows node
+def M2_HOME = tool 'maven-3.2.3'  // TODO fix 3.3.3 on windows node?
 def JAVA_HOME = tool 'jdk8'
 env.JAVA_HOME="${JAVA_HOME}"
 env.M2_HOME="${M2_HOME}"
 env.PATH = "${M2_HOME}/bin:${JAVA_HOME}/bin:${env.PATH}"
 
 // Get the zipfile name and home directory from the full download URL
-env.AMQ_KIT_URL = "${AMQ_KIT_URL}"
+//env.AMQ_KIT_URL = "${AMQ_KIT_URL}"  // TODO is this needed?
 
 def lastSlash = AMQ_KIT_URL.lastIndexOf("/");
 def zipFileName = AMQ_KIT_URL.substring(lastSlash + 1, AMQ_KIT_URL.length());
 def amqHome = zipFileName.substring(0, zipFileName.length() - 4);
 
-env.ZIPFILENAME="${zipFileName}"
-env.AMQ_HOME="${amqHome}"
-
-def unix = isUnix()
-
 stage 'cleanup from previous runs'
-if (unix) {
-    sh 'rm -rf jboss-a-mq*'
-} else {
-    bat 'rm -rf jboss-a-mq*'
-}
-
-// Download the kit and unzip it
-stage 'download'
-if(unix) {
-    sh 'wget --no-verbose ${AMQ_KIT_URL}'
-    sh 'unzip -q ${ZIPFILENAME}'
-} else {
-    bat 'wget --no-verbose --no-check-certificate %AMQ_KIT_URL%'
-    bat 'unzip -q %ZIPFILENAME%'
-}
-
-// 3. Uncomment admin user in etc/user.properties
-if (unix) {
-    sh 'sed -i \'s/^#admin/admin/g\' ${AMQ_HOME}/etc/users.properties'
-} else {
-    bat 'sed -i \'s/^#admin/admin/g\' %AMQ_HOME%/etc/users.properties'
-}
+cleanup("jboss-a-mq*")
+stage 'download kit'
+downloadKit(AMQ_KIT_URL, zipFileName)
+uncommentAdminuserPassword(amqHome);
 
 try {
     // 4. Start the broker
     stage 'starting broker'
-    if (unix) {
-        sh './${AMQ_HOME}/bin/start'
-    } else {
-        bat '%AMQ_HOME%\\bin\\start'
-    }
+    startBroker(amqHome)
     //  Wait for it to start -- search for "Broker amq has started." in log, or sleep
     sleep 90
 
     stage 'Part 1 of tests'
-    if (unix) {
-        sh 'mvn --version'
-        sh 'mvn -Dmaven.test.failure.ignore -DAMQ_USER=admin -DAMQ_PASSWORD=admin -DBROKER_URL="tcp://localhost:61616" clean test'
-    } else {
-        bat 'mvn --version'
-        bat 'mvn -Dmaven.test.failure.ignore -DAMQ_USER=admin -DAMQ_PASSWORD=admin -DBROKER_URL="tcp://localhost:61616" clean test'
-    }
+    maven('--version')
+    maven('-Dmaven.test.failure.ignore -DAMQ_USER=admin -DAMQ_PASSWORD=admin -DBROKER_URL="tcp://localhost:61616" clean test')
 
     stage 'Broker Restart'
-    if (unix) {
-        sh './${AMQ_HOME}/bin/stop'
-        sleep 30
-        sh './${AMQ_HOME}/bin/start'
-        sleep 60
-    } else {
-        bat '%AMQ_HOME%\\bin\\stop'
-        sleep 30
-        bat '%AMQ_HOME%\\bin\\start'
-        sleep 60
-    }
+    stopBroker(amqHome)
+    sleep 30
+    startBroker(amqHome)
+    sleep 60
 
     stage 'Part 2 of tests'
-    if (unix) {
-        sh 'mvn -PpartTwo -Dmaven.test.failure.ignore -DAMQ_USER=admin -DAMQ_PASSWORD=admin -DBROKER_URL="tcp://localhost:61616" test'
-    } else {
-        bat 'mvn -PpartTwo -Dmaven.test.failure.ignore -DAMQ_USER=admin -DAMQ_PASSWORD=admin -DBROKER_URL="tcp://localhost:61616" test'
-    }
+    maven('-PpartTwo -Dmaven.test.failure.ignore -DAMQ_USER=admin -DAMQ_PASSWORD=admin -DBROKER_URL="tcp://localhost:61616" clean test')
 } finally {
     stage 'Final shutdown'
-    if (unix) {
-        sh './${AMQ_HOME}/bin/stop'
-    } else {
-        bat '%AMQ_HOME%\\bin\\stop'
-    }
+    stopBroker(amqHome)
 
-    if (!unix) {
+    // TODO do this here, or in calling script?
+    if (!isUnix()) {
         build job: 'Reboot_windows', quietPeriod: 30, wait: false
     } else {
         stage 'clear out workspace'
         deleteDir()  //Looks like we can't do this on windows
     }
 }
+
+
+def cleanup(directoryName) {
+    if (isUnix()) {
+        echo "Running unix version"
+        sh 'rm -rf ' + directoryName
+    } else {
+        bat 'rm -rf ' + directoryName
+    }
+}
+
+def downloadKit(downloadUrl, zipFileName) {
+    if(isUnix()) {
+        sh 'wget --no-verbose ' + downloadUrl
+        sh 'unzip -q ' + zipFileName
+    } else {
+        bat 'wget --no-verbose --no-check-certificate ' + downloadUrl
+        bat 'unzip -q ' + zipFileName
+    }
+}
+
+def uncommentAdminuserPassword(amqHomeDirectory) {
+    if (isUnix()) {
+        sh 'sed -i \'s/^#admin/admin/g\' ' + amqHomeDirectory + '/etc/users.properties'
+    } else {
+        bat 'sed -i \'s/^#admin/admin/g\' ' + amqHomeDirectory + '/etc/users.properties'
+    }
+}
+
+def startBroker(amqHomeDirectory) {
+    if (isUnix()) {
+        sh './' + amqHomeDirectory + '/bin/start'
+    } else {
+        bat amqHomeDirectory + '\\bin\\start'
+    }
+}
+
+def stopBroker(amqHomeDirectory) {
+    if (isUnix()) {
+        sh './' + amqHomeDirectory + '/bin/stop'
+    } else {
+        bat amqHomeDirectory + '\\bin\\stop'
+    }
+}
+
+def maven(command) {
+    if (isUnix()) {
+        sh 'mvn ' + command
+    } else {
+        bat 'mvn ' + command
+    }
+}
+
